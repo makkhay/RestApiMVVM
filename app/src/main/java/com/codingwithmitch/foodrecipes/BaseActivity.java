fad5c13fc98c86c6803734b685cdc1e801aeb8e4
Prakash <makkhay@gmail.com>
2019-12-13 12:43:05 -0800

    test

--- app/src/main/java/com/codingwithmitch/foodrecipes/BaseActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/BaseActivity.java
@@ -2,6 +2,7 @@ package com.codingwithmitch.foodrecipes;
 
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
@@ -9,6 +10,8 @@ import android.widget.ProgressBar;
 public abstract class BaseActivity extends AppCompatActivity {
 
     public ProgressBar mProgressBar;
+
+    private static final String TAG = "BaseActivity";
 
     @Override
     public void setContentView(int layoutResID) {
@@ -23,5 +26,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public void showProgressBar(boolean visibility){
         mProgressBar.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+
+        Log.d(TAG, ": ");
     }
 }
